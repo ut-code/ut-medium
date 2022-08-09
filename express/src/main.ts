ec4d6c6fc3f799c7f490a665a29b1db65a880e30
kokhayas <1234.884.koki@gmail.com>
2022-08-10 00:57:03 +0900

    articles by kokhayas

--- express/src/main.ts
+++ express/src/main.ts
@@ -36,13 +36,44 @@ type User = {
     email: string
 };
 
-const users: User[] = [
-    { id: 1, name: "User1", email: "user1@test.local" },
-    { id: 2, name: "User2", email: "user2@test.local" },
-    { id: 3, name: "User3", email: "user3@test.local" }
-]
+// const users: User[] = [
+//     { id: 1, name: "User1", email: "user1@test.local" },
+//     { id: 2, name: "User2", email: "user2@test.local" },
+//     { id: 3, name: "User3", email: "user3@test.local" }
+// ]
+// class Article{
+//   id: number;
+//   title: string = "";
+//   author: string = "";
+//   content: string = "";
+//   // avatar: ??? = ???;
+// }
+
+// let articles: Article[] = [
+//     {
+//     id: 0,
+//     title: "吾輩は猫である",
+//     author: "夏目漱石",
+//     content: "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n本プロジェクトでは,不完全ゲームの一種である人狼ゲームをプレイするAI「人狼知能」を対象として,不完全情報協力ゲームにおける人工知能の開発を目指す.特に,単にゲームをプレイするAIの開発だけではなく,自然言語による自然な対話の実現, AIと人間プレイヤーとの協調等高度なAI技術の実現を目指す."
+//   },
+//   {
+//     id: 1,
+//     title: "君の名は",
+//     author: "Shinkai Makoto",
+//     content: "東京の四ツ谷[注 2]に暮らす男子高校生・立花 瀧（たちばな たき）は、ある朝、目を覚ますと飛騨地方[13]の山深い田舎町である糸守町[注 3]に住む女子高生で宮水神社の巫女を務める宮水 三葉（みやみず みつは）になっており、逆に三葉は瀧になっていた。2人とも「奇妙な夢」だと思いながら、知らない誰かの一日を過ごす。"
+//   },
+//   {
+//     id: 2,
+//     title: "The Law of Return and the Right of Return",
+//     author: "Michael Sandel",
+//     content: "What it means to be free is to recognize certain moral ties we haven't chosen, ties bound up with history, membership, memory, then it's hard to separate the idea of obligations that point inward sometimes in and sometimes outward."
+//   }]
 
 //一覧取得
 app.get('/users', (req: express.Request, res: express.Response) => {
     res.send(JSON.stringify(users))
 })
+
+// app.get('/article:id', (req: express.Request, res: express.Response) => {
+//   res.send(articles[req.params.id].content)
+// })
--- react-nextjs/pages/index.tsx
+++ react-nextjs/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next';
 // import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 import { useState } from 'react';
 
-
 class Article{
+  id: number;
   title: string = "";
   author: string = "";
   content: string = "";
@@ -20,16 +21,19 @@ class Review{
 const Home: NextPage = () => {
   const [articles, setArticles] = useState<Article[]>([
     {
+    id: 0,
     title: "吾輩は猫である",
     author: "夏目漱石",
     content: "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n本プロジェクトでは,不完全ゲームの一種である人狼ゲームをプレイするAI「人狼知能」を対象として,不完全情報協力ゲームにおける人工知能の開発を目指す.特に,単にゲームをプレイするAIの開発だけではなく,自然言語による自然な対話の実現, AIと人間プレイヤーとの協調等高度なAI技術の実現を目指す."
   },
   {
+    id: 1,
     title: "君の名は",
     author: "Shinkai Makoto",
     content: "東京の四ツ谷[注 2]に暮らす男子高校生・立花 瀧（たちばな たき）は、ある朝、目を覚ますと飛騨地方[13]の山深い田舎町である糸守町[注 3]に住む女子高生で宮水神社の巫女を務める宮水 三葉（みやみず みつは）になっており、逆に三葉は瀧になっていた。2人とも「奇妙な夢」だと思いながら、知らない誰かの一日を過ごす。"
   },
   {
+    id: 2,
     title: "The Law of Return and the Right of Return",
     author: "Michael Sandel",
     content: "What it means to be free is to recognize certain moral ties we haven't chosen, ties bound up with history, membership, memory, then it's hard to separate the idea of obligations that point inward sometimes in and sometimes outward."
@@ -54,11 +58,11 @@ const Home: NextPage = () => {
         </div>
 
         <div className="">
-          <div>一覧</div>
+          <div>学内 主要</div>
           {articles.map((article, index) => {
             return (
               <div key={index}>
-                <div className="text-sky-500 dark:text-sky-400">{article.title}</div>
+                <Link href={{pathname:"/article", query: {id: article.id}}}><div className="text-sky-500 dark:text-sky-400">{article.title}</div></Link>
                 <div className="text-orange-500 dark:text-orange-400">{article.author}</div>
                 <div>{article.content}</div>
                 <br></br>
